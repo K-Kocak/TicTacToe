71af03166a9b85655e4c3d02b5cddca5afa485ac
K-Kocak <kakocak@outlook.com>
2024-05-19 21:00:36 +0100

    Test Commit

--- TicTacToe/TicTacToe/obj/Release/net6.0/TicTacToe.AssemblyInfo.cs
+++ TicTacToe/TicTacToe/obj/Release/net6.0/TicTacToe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicTacToe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a40605b9fbeb1176b586c876c676506c90e4026d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicTacToe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicTacToe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
